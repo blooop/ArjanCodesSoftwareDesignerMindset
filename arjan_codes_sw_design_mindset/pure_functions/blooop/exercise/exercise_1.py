d39e3e338dcadcf32f125815610d205af2425ece
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-25 09:46:55 +0000

    fix linting

--- arjan_codes_sw_design_mindset/pure_functions/blooop/exercise/exercise_1.py
+++ arjan_codes_sw_design_mindset/pure_functions/blooop/exercise/exercise_1.py
@@ -3,12 +3,12 @@ import string
 from datetime import datetime
 
 
-def generate_id(length: int,seed:int=0) -> str:
+def generate_id(length: int, seed: int = 0) -> str:
     random.seed(seed)
     return "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
 
-def weekday(today:datetime) -> str:    
+def weekday(today: datetime) -> str:
     return f"{today:%A}"
 
 
--- arjan_codes_sw_design_mindset/pure_functions/blooop/exercise/exercise_2.py
+++ arjan_codes_sw_design_mindset/pure_functions/blooop/exercise/exercise_2.py
@@ -16,25 +16,27 @@ class Laptop:
 
     def factory_reset(self):
         self.format_hd()
-        self.machine_name ="DULL"
+        self.machine_name = "DULL"
         self.install_os()
         self.create_admin_user("admin")
 
-def factory_reset(laptop:Laptop):
+
+def factory_reset(laptop: Laptop):
     laptop.format_hd()
     laptop.machine_name = "DULL"
     laptop.install_os()
     laptop.create_admin_user("admin")
     return laptop
 
+
 def main() -> None:
-    laptop = Laptop().factory_reset()
+    laptop = Laptop()
+    laptop.factory_reset()
     print(laptop)
 
     laptop2 = factory_reset(Laptop())
 
     print(laptop2)
-
 
 
 if __name__ == "__main__":
