9a4f101112a7372fb472f08247c0113b44d85847
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-09-01 13:33:04 +0000

    remove dependency on smtp

--- arjan_codes_sw_design_mindset/depend_on_abstractions/blooop/exercise_2.py
+++ arjan_codes_sw_design_mindset/depend_on_abstractions/blooop/exercise_2.py
@@ -1,4 +1,7 @@
-from smtplib import SMTP
+# from smtplib import SMTP
+
+from typing import Protocol
+
 
 DEFAULT_EMAIL = "support@arjancodes.com"
 LOGIN = "test"
@@ -6,9 +9,21 @@ PASSWORD = "my_password"
 HOST = "smtp.arjancodes.com"
 PORT = 19584
 
+class SMPTInterface(Protocol):
 
-def send_email(message: str, to_address: str, from_address: str = DEFAULT_EMAIL) -> None:
-    server = SMTP()
+    def connect(self,host:str,port:int)->None:
+        ...
+
+    def login(self,login:str,password:str)->None:
+        ...
+
+    def sendmail(self,from_address:str,to_address:str,message:str)->None:
+        ...
+    
+    def quit(self)->None:
+        ...
+
+def send_email(server:SMPTInterface, message: str, to_address: str, from_address: str = DEFAULT_EMAIL) -> None:
     server.connect(HOST, PORT)
     server.login(LOGIN, PASSWORD)
     server.sendmail(from_address, to_address, message)
