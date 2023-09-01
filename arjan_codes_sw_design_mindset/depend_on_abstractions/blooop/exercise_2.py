cf500f6b1387aa7dddae91939dfcb22532172dad
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-09-01 13:33:19 +0000

    autoformat

--- arjan_codes_sw_design_mindset/depend_on_abstractions/blooop/exercise_2.py
+++ arjan_codes_sw_design_mindset/depend_on_abstractions/blooop/exercise_2.py
@@ -9,21 +9,24 @@ PASSWORD = "my_password"
 HOST = "smtp.arjancodes.com"
 PORT = 19584
 
+
 class SMPTInterface(Protocol):
-
-    def connect(self,host:str,port:int)->None:
+    def connect(self, host: str, port: int) -> None:
         ...
 
-    def login(self,login:str,password:str)->None:
+    def login(self, login: str, password: str) -> None:
         ...
 
-    def sendmail(self,from_address:str,to_address:str,message:str)->None:
-        ...
-    
-    def quit(self)->None:
+    def sendmail(self, from_address: str, to_address: str, message: str) -> None:
         ...
 
-def send_email(server:SMPTInterface, message: str, to_address: str, from_address: str = DEFAULT_EMAIL) -> None:
+    def quit(self) -> None:
+        ...
+
+
+def send_email(
+    server: SMPTInterface, message: str, to_address: str, from_address: str = DEFAULT_EMAIL
+) -> None:
     server.connect(HOST, PORT)
     server.login(LOGIN, PASSWORD)
     server.sendmail(from_address, to_address, message)
