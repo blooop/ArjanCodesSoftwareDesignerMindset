b68db8ffca796c6d3b9f7bab21e38ddef6a54f21
Giggus <giulio.cerruti@gmail.com>
2023-09-06 14:52:29 +0000

    [Updated] CI checked

--- arjan_codes_sw_design_mindset/depend_on_abstractions/giggus/exercise_1.py
+++ arjan_codes_sw_design_mindset/depend_on_abstractions/giggus/exercise_1.py
@@ -5,8 +5,11 @@ Retrieve = Callable[[], str]
 Connect = Callable[[], None]
 Initialize = Callable[[str], None]
 
-class CloudService():
-    def connect(self, credentials: Retrieve, connect: Connect, context: Retrieve, initialize: Initialize) -> None:
+
+class CloudService:
+    def connect(
+        self, credentials: Retrieve, connect: Connect, context: Retrieve, initialize: Initialize
+    ) -> None:
         print("Connecting to the cloud service.")
         connect(credentials)
         initialize(context)
--- arjan_codes_sw_design_mindset/depend_on_abstractions/giggus/exercise_2.py
+++ arjan_codes_sw_design_mindset/depend_on_abstractions/giggus/exercise_2.py
@@ -6,22 +6,26 @@ PASSWORD = "my_password"
 HOST = "smtp.arjancodes.com"
 PORT = 19584
 
+
 class Server(Protocol):
     default_email: str
 
-    def connect(self, host: str, port: int)-> None:
+    def connect(self, host: str, port: int) -> None:
         ...
 
     def login(self, user_name: str, password: str) -> None:
         ...
-    
+
     def send_mail(self, message: str, to_address: str, from_address: str) -> None:
         ...
 
     def quit(self) -> None:
         ...
 
-def send_email(server: Server, message: str, to_address: str, from_address: str = DEFAULT_EMAIL) -> None:
+
+def send_email(
+    server: Server, message: str, to_address: str, from_address: str = DEFAULT_EMAIL
+) -> None:
     server.connect(HOST, PORT)
     server.login(LOGIN, PASSWORD)
     server.sendmail(from_address, to_address, message)
